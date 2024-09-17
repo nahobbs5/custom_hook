7e65901839afc440f487f80c6a12bcf11978067f
nahobbs5 <nahobbs5@gmail.com>
2024-09-16 20:51:33 -0400

    finished UseFetch lab

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,18 @@
+import { useState, useEffect} from "react";
+
+// url parameter lets you specify exact url of website.
+// Allows components to provide URL dynamically
 const UseFetch = (url) => {
-    
+    const[data, setData] = useState();
+    //used for performing side effects in fuctional components
+    useEffect(() => (
+        //initiate HTTP request to specified url and convert response to JSON
+        //setData updates state in React functional components
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    ),[])
+
+return[data]
 }
 
 export default UseFetch
